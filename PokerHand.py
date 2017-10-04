8b10b3e07607ed5ec3759924951a0b4c15f25f9e
Sinan Tang <tangsinan92@gmail.com>
2017-10-04 15:44:14 +0200

    finished bridge game exercise in think python ch18

--- PokerHand.py
+++ PokerHand.py
@@ -11,11 +11,11 @@ class Hist(dict):
         if self[x] == 0: del self[x]
 
 
-
 class PokerHand(Hand):
     """Represents a poker hand."""
 
-    all_labels = ['straightflush', 'fourkind', 'fullhouse', 'flush', 'straight', 'threekind', 'twopair', 'pair', 'highcard']
+    all_labels = ['straightflush', 'fourkind', 'fullhouse', 'flush', 'straight', 'threekind', 'twopair', 'pair']
+    # 'highcard'
 
     def make_histograms(self):
         self.suits = Hist()
@@ -23,40 +23,42 @@ class PokerHand(Hand):
 
         for c in self.cards:
             self.suits.count(c.suit)
-            self.suits.count(c.rank)
+            self.ranks.count(c.rank)
 
         self.sets = list(self.ranks.values())
         self.sets.sort(reverse=True)
 
-    def suit_hist(self):
-        """Builds a histogram of the suits that appear in the hand.
-        Stores the result in attribute suits.
-        """
-        self.suits = {}
-        for card in self.cards:
-            self.suits[card.suit] = self.suits.get(card.suit, 0) + 1
+    # def suit_hist(self):
+    #     """Builds a histogram of the suits that appear in the hand.
+    #     Stores the result in attribute suits.
+    #     """
+    #     self.suits = {}
+    #     for card in self.cards:
+    #         self.suits[card.suit] = self.suits.get(card.suit, 0) + 1
+    # def rank_hist(self):
+    #     """Builds a histogram of the ranks that appear in the hand.
+    #     Stores the result in attribute ranks.
+    #     """
+    #     self.ranks = {}
+    #     for card in self.cards:
+    #         self.ranks[card.rank] = self.ranks.get(card.rank, 0) + 1
 
-    def rank_hist(self):
-        """Builds a histogram of the ranks that appear in the hand.
-        Stores the result in attribute ranks.
-        """
-        self.ranks = {}
-        for card in self.cards:
-            self.ranks[card.rank] = self.ranks.get(card.rank, 0) + 1
+    # def has_highcard(self):
+    #     return len(self.cards)
 
     def has_flush(self):
         """Returns True if the hand has a flush, False otherwise.
 
         Note that this works correctly for hands with more than 5 cards.
         """
-        self.suit_hist()
+        self.make_histograms()
         for val in self.suits.values():
             if val >= 5:
                 return True
         return False
 
     def same_rank(self, card_num=2, pair_num=1):
-        self.rank_hist()
+        self.make_histograms()
         count = 0
         for val in self.ranks.values():
             if val >= card_num:
@@ -67,28 +69,28 @@ class PokerHand(Hand):
             return False
 
     def same_rank_exact(self, card_num=2):
-        self.rank_hist()
+        self.make_histograms()
         for val in self.ranks.values():
             if val == card_num: return True
         return False
 
     def has_pair(self):
-        return self.same_rank(2,1)
+        return self.same_rank(2, 1)
 
     def has_twopair(self):
-        return self.same_rank(2,2)
+        return self.same_rank(2, 2)
 
-    def has_three_of_a_kind(self):
+    def has_threekind(self):
         return self.same_rank(3, 1)
 
-    def has_four_of_a_kind(self):
+    def has_fourkind(self):
         return self.same_rank(4, 1)
 
-    def has_full_house(self):
+    def has_fullhouse(self):
         return self.same_rank_exact(3) and self.same_rank_exact(2)
 
     def has_straight0(self):
-        self.rank_hist()
+        self.make_histograms()
         key_ls = list(self.ranks.keys())
         key_ls.sort()
         # print(key_ls)
@@ -101,8 +103,9 @@ class PokerHand(Hand):
         return False
 
     def has_straight(self):
+        self.make_histograms()
         ranks = self.ranks.copy()
-        print('In has_straight, ranks:', ranks) # empty {}
+        # print('In has_straight, ranks:', ranks)
         ranks[14] = ranks.get(1, 0)
         return self.in_a_row(ranks, 5)
 
@@ -110,6 +113,7 @@ class PokerHand(Hand):
         count = 0
         for i in range(1, 15):
             if ranks.get(i, 0):
+            # when a ranks[i] doesnt exist, this branch is skipped and count=0
                 count += 1
                 if count == n:
                     return True
@@ -132,44 +136,104 @@ class PokerHand(Hand):
         return False
 
     def has_straightflush(self):
+        # smart algorithm
         # partition the hand by suit and check each sub-hand for a straight
         d = {}
         for c in self.cards:
             d.setdefault(c.suit, PokerHand()).add_card(c)
-        # print(d.values())
+
+        # for k,v in d.items():
+        #     print(k,',',v,'\n')
 
         # to see if any of the partitioned hands has a straight
         for hand in d.values():
-            print(hand, '\n')
+            # print(hand, '\n')
             if len(hand.cards) < 3:
                 continue
-            hand.make_histograms()
             if hand.has_straight():
                 return True
         return False
 
+    def classify(self):
+        self.labels = []
+        for label in PokerHand.all_labels:
+            # getattr(self, STR) == self.STR
+            f = getattr(self, 'has_'+label)
+            if f():
+                self.labels.append(label)
+
+        # if self.has_pair():
+        #     self.label = self.all_labels[7]
+        #     return
 
 
-if __name__ == '__main__':
-    # make a deck
+def PokerDeck():
     deck = Deck()
     deck.shuffle()
 
-    # deal the cards and classify the hands
-    for i in range(1):
-    # range_number * num of cards in a hand <= 53 cards..
+    d = {}
+    for i in range(7):
         hand = PokerHand()
-        deck.move_cards(hand, 10)
+        deck.move_cards(hand, 7)
         hand.sort()
-        print(hand)
-        print()
+        # print(hand)
+        # print()
+        hand.classify()
+        for label in hand.labels:
+            d[label] = d.get(label, 0) + 1
+
+    # calculates the probability of the classifications
+    # total = sum(d.values())
+    # for key in d:
+    #     d[key] = (d[key], d[key]/total)
+    return d
+
+
+
+def main(n=10):
+    total_dict = {}
+
+    for i in range(n):
+        d = PokerDeck()
+        for label in d:
+            total_dict[label] = total_dict.get(label, 0) + 1
+
+    total_hands = 7.0 * n
+    for label in total_dict:
+        freq = total_dict[label]/total_hands
+        print('{} has a probability of {:.3f}% to happen.\n'.format(label, freq))
+    # return total_dict
+
+
+if __name__ == '__main__':
+    main(1000)
+
+
+
+# if __name__ == '__main__':
+#     # make a deck
+#     deck = Deck()
+#     deck.shuffle()
+#
+#     # deal the cards and classify the hands
+#     for i in range(2):
+#     # range_number * num of cards in a hand <= 53 cards..
+#         hand = PokerHand()
+#         deck.move_cards(hand, 10)
+#         hand.sort()
+        # print(hand)
+        # print()
         # print('Flush:', hand.has_flush())
         # print('Pair:', hand.has_pair())
         # print('Two Pair:', hand.has_twopair())
-        # print('Three of a kind:', hand.has_three_of_a_kind())
-        # print('Four of a kind:', hand.has_four_of_a_kind())
-        # print('Full House:', hand.has_full_house())
-        print('Straight:', hand.has_straight())
-        print('Straight Flush:', hand.has_straightflush())
-        print()
-        print()
+        # print('Three of a kind:', hand.has_threekind())
+        # print('Four of a kind:', hand.has_fourkind())
+        # print('Full House:', hand.has_fullhouse())
+        # print('Straight:', hand.has_straight())
+        # print('Straight Flush:', hand.has_straightflush())
+        # print()
+        # print()
+        # hand.classify()
+        # print(hand.labels)
+        # print()
+        # print()
